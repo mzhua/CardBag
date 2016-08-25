79941e0a213389b1c2852fb273ec2d5addc32304
hua <mzhua78@hotmail.com>
2016-08-25 12:54:11 +0800

    add ucrop

--- card-bag/src/main/java/com/wonders/xlab/cardbag/ui/cardedit/CardEditActivity.java
+++ card-bag/src/main/java/com/wonders/xlab/cardbag/ui/cardedit/CardEditActivity.java
@@ -1,36 +1,42 @@
 package com.wonders.xlab.cardbag.ui.cardedit;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.wonders.xlab.cardbag.R;
+import com.wonders.xlab.cardbag.base.BaseContract;
+import com.wonders.xlab.cardbag.base.MVPActivity;
+import com.wonders.xlab.cardbag.util.FileUtil;
 import com.wonders.xlab.cardbag.util.ImageViewUtil;
 import com.wonders.xlab.cardbag.widget.RatioImageView;
 import com.wonders.xlab.cardbag.widget.TopBar;
 import com.wonders.xlab.qrscanner.BarCodeEncoder;
 import com.wonders.xlab.qrscanner.XQrScanner;
+import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class CardEditActivity extends Activity {
+import static android.R.attr.maxHeight;
+import static android.R.attr.maxWidth;
+
+public class CardEditActivity extends MVPActivity {
     private final int REQUEST_CODE_MASK = 1;
     private final int REQUEST_CODE_SCAN_BAR_CODE = REQUEST_CODE_MASK << 1;
-    private final int REQUEST_TAKE_PHOTO_CARD_FRONT = REQUEST_CODE_MASK << 2;
-    private final int REQUEST_TAKE_PHOTO_CARD_BACK = REQUEST_CODE_MASK << 3;
+    private final int REQUEST_CODE_TAKE_PHOTO_CARD_FRONT = REQUEST_CODE_MASK << 2;
+    private final int REQUEST_CODE_TAKE_PHOTO_CARD_BACK = REQUEST_CODE_MASK << 3;
+    private final int REQUEST_CODE_CROP_FRONT = REQUEST_CODE_MASK << 4;
+    private final int REQUEST_CODE_CROP_BACK = REQUEST_CODE_MASK << 5;
 
 
     private TopBar mTopBar;
@@ -40,6 +46,7 @@ public class CardEditActivity extends Activity {
     private RatioImageView mIvBarCode;
     private EditText mEtCardName;
     private TextView mTvBarCode;
+    private ImageView mIvClear;
 
     private String mCardId;
 
@@ -47,6 +54,13 @@ public class CardEditActivity extends Activity {
     private Bitmap mBitmapBarCodeTmp;
     private String mCardFrontPhotoPath;
     private String mCardBackPhotoPath;
+
+    private UCrop.Options mOptions;
+
+    @Override
+    protected BaseContract.Presenter getPresenter() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +73,16 @@ public class CardEditActivity extends Activity {
         mIvBarCode = (RatioImageView) findViewById(R.id.iv_bar_code);
         mEtCardName = (EditText) findViewById(R.id.et_card_name);
         mTvBarCode = (TextView) findViewById(R.id.tv_bar_code);
+        mIvClear = (ImageView) findViewById(R.id.iv_clear);
 
+        setupViewListener();
+
+        initialWithExtra();
+
+        initUCropOptions();
+    }
+
+    private void initialWithExtra() {
         Intent intent = getIntent();
         if (intent != null) {
             mCardId = intent.getStringExtra("id");
@@ -75,9 +98,52 @@ public class CardEditActivity extends Activity {
         mTopBar.setTitle(TextUtils.isEmpty(mCardId) ? getString(R.string.title_card_edit_add) : getString(R.string.title_card_edit_edit));
     }
 
+    private void setupViewListener() {
+        mEtCardName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mIvClear.setVisibility(s.length() <= 0 ? View.INVISIBLE : View.VISIBLE);
+            }
+        });
+        mIvClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEtCardName.setText("");
+            }
+        });
+    }
+
+    private void initUCropOptions() {
+        if (mOptions == null) {
+            mOptions = new UCrop.Options();
+            int color = getResources().getColor(R.color.topBarBackground);
+            mOptions.setToolbarColor(color);
+            mOptions.setToolbarWidgetColor(getResources().getColor(R.color.textBlack));
+            mOptions.setStatusBarColor(getResources().getColor(R.color.textBlack));
+            mOptions.setLogoColor(getResources().getColor(R.color.textBlack));
+        }
+    }
+
     public void scanBarCode(View view) {
         XQrScanner.getInstance()
                 .startForResult(this, REQUEST_CODE_SCAN_BAR_CODE);
+    }
+
+    public void shotCardFront(View view) {
+        dispatchTakePictureIntent("front", REQUEST_CODE_TAKE_PHOTO_CARD_FRONT);
+    }
+
+    public void shotCardBack(View view) {
+        dispatchTakePictureIntent("back", REQUEST_CODE_TAKE_PHOTO_CARD_BACK);
     }
 
     @Override
@@ -96,68 +162,32 @@ public class CardEditActivity extends Activity {
                         e.printStackTrace();
                     }
                     break;
-                case REQUEST_TAKE_PHOTO_CARD_FRONT:
-                    setPic(mIvCardFront, mCardFrontPhotoPath);
+                case REQUEST_CODE_TAKE_PHOTO_CARD_FRONT:
+                    UCrop.of(Uri.parse("file:" + mCardFrontPhotoPath), Uri.parse("file:" + mCardFrontPhotoPath))
+                            .withAspectRatio(3, 2)
+                            .withMaxResultSize(maxWidth, maxHeight)
+                            .withOptions(mOptions)
+                            .start(this, REQUEST_CODE_CROP_FRONT);
                     break;
-                case REQUEST_TAKE_PHOTO_CARD_BACK:
-                    setPic(mIvCardBack, mCardBackPhotoPath);
+                case REQUEST_CODE_TAKE_PHOTO_CARD_BACK:
+                    UCrop.of(Uri.parse("file:" + mCardBackPhotoPath), Uri.parse("file:" + mCardBackPhotoPath))
+                            .withAspectRatio(3, 2)
+                            .withMaxResultSize(maxWidth, maxHeight)
+                            .withOptions(mOptions)
+                            .start(this, REQUEST_CODE_CROP_BACK);
+                    break;
+                case REQUEST_CODE_CROP_FRONT:
+                    mIvCardFront.setImageURI(UCrop.getOutput(data));
+                    break;
+                case REQUEST_CODE_CROP_BACK:
+                    mIvCardBack.setImageURI(UCrop.getOutput(data));
+                    break;
+                case UCrop.RESULT_ERROR:
+                    final Throwable cropError = UCrop.getError(data);
+                    showShortToast(cropError.getMessage());
                     break;
             }
         }
-    }
-
-    public void shotCardFront(View view) {
-        dispatchTakePictureIntent("front", REQUEST_TAKE_PHOTO_CARD_FRONT);
-    }
-
-    public void shotCardBack(View view) {
-        dispatchTakePictureIntent("back", REQUEST_TAKE_PHOTO_CARD_BACK);
-    }
-
-    private void setPic(ImageView imageView, String photoPath) {
-        // Get the dimensions of the View
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(photoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, bmOptions);
-        imageView.setImageBitmap(bitmap);
-    }
-
-    private File createImageFile(String fileName, int requestCode) throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = fileName + "_JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        switch (requestCode) {
-            case REQUEST_TAKE_PHOTO_CARD_FRONT:
-                mCardFrontPhotoPath = image.getAbsolutePath();
-                break;
-            case REQUEST_TAKE_PHOTO_CARD_BACK:
-                mCardBackPhotoPath = image.getAbsolutePath();
-                break;
-        }
-        return image;
     }
 
     private void dispatchTakePictureIntent(String fileName, int requestCode) {
@@ -167,16 +197,27 @@ public class CardEditActivity extends Activity {
             // Create the File where the photo should go
             File photoFile = null;
             try {
-                photoFile = createImageFile(fileName, requestCode);
+                photoFile = FileUtil.createFile(this, fileName);
+                if (photoFile == null) {
+                    showShortToast("保存图片失败,请确认是否授权读写存储卡的权限以及存储卡是否正确挂载");
+                    return;
+                }
+                switch (requestCode) {
+                    case REQUEST_CODE_TAKE_PHOTO_CARD_FRONT:
+                        mCardFrontPhotoPath = photoFile.getAbsolutePath();
+                        break;
+                    case REQUEST_CODE_TAKE_PHOTO_CARD_BACK:
+                        mCardBackPhotoPath = photoFile.getAbsolutePath();
+                        break;
+                }
             } catch (IOException ex) {
                 // Error occurred while creating the File
+                showShortToast("保存图片失败,请确认是否授权读写存储卡的权限以及存储卡是否正确挂载");
             }
             // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = Uri.fromFile(photoFile);//FileProvider.getUriForFile(this,"com.wonders.xlab.cardbag.fileprovider",photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, requestCode);
-            }
+            Uri photoURI = Uri.fromFile(photoFile);//FileProvider.getUriForFile(this,"com.wonders.xlab.cardbag.fileprovider",photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, requestCode);
         }
     }
 }
--- card-bag/src/main/java/com/wonders/xlab/cardbag/util/FileUtil.java
+++ card-bag/src/main/java/com/wonders/xlab/cardbag/util/FileUtil.java
@@ -2,14 +2,23 @@ package com.wonders.xlab.cardbag.util;
 
 import android.content.Context;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by hua on 16/8/24.
  */
 
 public class FileUtil {
+
+    private static final String ERROR_CREATE_FILE_FAILED = "保存文件失败,请确认是否授权读写存储卡的权限以及存储卡是否正确挂载";
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
     public static boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED.equals(state);
@@ -21,17 +30,26 @@ public class FileUtil {
                 Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
     }
 
-    public static File createFile(Context context, String fileName) {
+    public static File createFile(Context context, String fileName) throws IOException {
         if (isExternalStorageWritable()) {
             // Get the directory for the app's private pictures directory.
-            File file = new File(Environment.getExternalStorageDirectory(), context.getApplicationInfo().name);
-            if (!file.mkdirs()) {
-
-            }
-
-            return file;
+            File file = getInternalDirectory(context);
+            return new File(file.getAbsolutePath() + File.separator + fileName + DATE_FORMAT.format(new Date()));
         }
-
+        Toast.makeText(context, ERROR_CREATE_FILE_FAILED, Toast.LENGTH_SHORT).show();
         return null;
+    }
+
+    @NonNull
+    private static File getPublicDirectory() {
+        File file = new File(Environment.getExternalStorageDirectory(), "cardbag");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file;
+    }
+
+    private static File getInternalDirectory(Context context) {
+        return context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
     }
 }
